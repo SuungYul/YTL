7d4bb37f2ac8bc880e2f23aed2516407ed621c2b
SuungYul <pmtf00@gmail.com>
2022-11-24 02:51:37 +0900

    파이어베이스 연동 마커 찍기 성공

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import { Map } from "./map";
-import { RenderAfterNavermapsLoaded } from 'react-naver-maps';
+import Map from "./Map/map";
+import { RenderAfterNavermapsLoaded } from "react-naver-maps";
 const App = () => {
-    let mapLat = "37.23229";
-    let mapLng = "127.188205";
-    const YOUR_CLIENT_ID = "w4msaekuxw"
-    return (
-        <div className="row mt-1" style={{ height: "300px;" }}>
-            <RenderAfterNavermapsLoaded ncpClientId={YOUR_CLIENT_ID}>
-                <Map mapLat={mapLat} mapLng={mapLng} />
-            </RenderAfterNavermapsLoaded>
-        </div>
-    );
-}
+  let mapLat = "37.23229";
+  let mapLng = "127.188205";
+
+  return (
+    <div className="row mt-1">
+      <Map mapLat={mapLat} mapLng={mapLng} />
+    </div>
+  );
+};
 
 export default App;
-
-
-
-
-
-
-
-
---
+++ src/Map/map.js
@@ -0,0 +1,105 @@
+import { getDoc } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
+import { NaverMap, Marker } from "react-naver-maps";
+import { RenderAfterNavermapsLoaded } from "react-naver-maps";
+import { Crosswalk } from "../database/data";
+import { getPosition, getData, db, getDocs } from "../database/firebase";
+
+const Map = ({ mapLat, mapLng }) => {
+  let index = 0;
+
+  const YOUR_CLIENT_ID = "w4msaekuxw";
+  const [result, setResult] = useState([]);
+  const [totalDB, setTotalDB] = useState([]);
+  const [isLoad, setLoad] = useState(false);
+  useEffect(() => {
+    index = 0;
+    const totalDBPromise = getDocs("crosswalk");
+    totalDBPromise.then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        if (!totalDB.includes(doc.data())) {
+          setTotalDB((prevList) => [...prevList, doc.data()]);
+        }
+      });
+    });
+
+    totalDB.forEach((value) => {
+      const marker = MakeMarker(value);
+      console.log(marker);
+      setResult((prevResult) => [...prevResult, marker]);
+    });
+    setLoad(true);
+  }, []);
+
+  const MakeMarker = (db) => {
+    if (!db) {
+      console.log("Not db!");
+      return;
+    }
+    const content = {
+      content: [
+        '<div class="cs_mapbridge">',
+        '<div class="map_group _map_group">',
+        '<div class="map_marker _marker tvhp tvhp_big">',
+        '<span class="ico _icon"></span>',
+        '<span class="shd">',
+        db.cycle,
+        "</span>",
+        "</div>",
+        "</div>",
+        "</div>",
+      ].join(""),
+    };
+
+    const markers = [];
+    for (let i = 0; i < db.position.length; i++) {
+      const pos = {
+        // 이새끼 절때 for문 밖으로 내보내지말것
+        lat: 0,
+        lng: 0,
+      };
+      pos.lat = db.position[i]._lat;
+      pos.lng = db.position[i]._long;
+      markers.push(
+        <Marker
+          key={index++}
+          position={pos}
+          onClick={() => {
+            alert(pos);
+          }}
+        />
+      );
+    }
+    return markers;
+  };
+  return isLoad ? (
+    <>
+      <RenderAfterNavermapsLoaded
+        ncpClientId={YOUR_CLIENT_ID}
+        error={<p>Maps Load Error</p>}
+        loading={<p>Maps Loading...</p>}
+      >
+        <NaverMap
+          id="react-naver-maps"
+          style={{ width: "100%", height: "100vh" }}
+          center={{ lat: mapLat, lng: mapLng }}
+          defaultZoom={16}
+        >
+          {result}
+          <Marker
+            position={{ lat: mapLat, lng: mapLng }}
+            onClick={() => {
+              alert("여기는 입구입니다");
+            }}
+
+            // clickable={true}
+          />
+        </NaverMap>
+      </RenderAfterNavermapsLoaded>
+    </>
+  ) : (
+    "Loading..."
+  );
+};
+
+export default Map;
